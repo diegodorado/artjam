6375787a1ee0b17d4f19ee89520afb4c572c5051
Diego Dorado <diegodorado@gmail.com>
2021-12-03 04:45:46 -0300

    Add title and text animation

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 import intro from "./intro.json"
+import cowsay from "cowsay-browser"
+
 
 const raf = window.requestAnimationFrame
 
@@ -16,6 +18,36 @@ const maxFPS = 10
 let lastFrameTimeMs = 0
 let frameIndex = 0
 
+let talking = false
+let talkingFrame = 0
+
+const faces = ['bud-frogs', 'bunny', 'cheese', 'chick', 'cower', 'daemon', 'default', 'doge', 'dragon-and-cow', 'dragon', 'elephant-in-snake', 'elephant', 'eyes', 'flaming-sheep', 'kiss', 'kitty', 'koala', 'kosh', 'luke-koala', 'mech-and-cow', 'meow', 'moofasa', 'moose', 'ren', 'rooster', 'satanic', 'sheep', 'skeleton', 'small', 'squirrel', 'stegosaurus', 'stimpy', 'supermilker', 'turkey', 'turtle', 'tux', 'vader-koala', 'vader', 'www']
+let face = faces[Math.floor(Math.random()*faces.length)]
+
+// https://patorjk.com/software/taag/#p=display&f=AMC%203%20Liv1&t=Festival%20Placer
+const title = `
+             (                                (
+             )# )          )             (    )# ) (
+            (()/(  (    ( /((   )      ) )#  (()/( )#   )       (  (
+             /(_))))#(  )#())# /((  ( /(((_)  /(_)|(_| /(  (   ))# )(
+            (_))_/((_)#(_))((_|_))# )(_))_   (_))  _ )(_)) )# /((_|()#
+            | |_(_))((_) |_ (_))((_|(_)_| |  | _ #| ((_)_ ((_|_))  ((_)
+            | __/ -_|_-<  _|| # V // _* | |  |  _/| / _* / _|/ -_)| '_|
+            |_| #___/__/#__||_|#_/ #__,_|_|  |_|  |_#__,_#__|#___||_|
+
+`.replaceAll('#','\\').replaceAll('*','`')
+
+const text =  `buenas, como estan?
+...
+algo así me imagino el espacio.
+un anfitrión que converse
+con el visitante
+.
+les gusta mas blanco o verde
+el texto?
+`
+
+
 const update = (delta) => {
 }
 
@@ -24,6 +56,36 @@ const draw = () => {
   const frame = intro[index]
 
   let s = ""
+
+  if(talking) {
+
+    s += title
+
+    const options = {
+      text: text.substr(0,talkingFrame+1),
+      f: face,
+      //f: 'satanic', // Template for a cow, get inspiration from `./cows`
+      d: true,
+      //eyes: 'oo', // Select the appearance of the cow's eyes, equivalent to cowsay -e
+      // One of 	"b", "d", "g", "p", "s", "t", "w", "y"
+    }
+
+    let cow = cowsay.say(options)
+    s += `/${'-'.repeat(80)}\\\n`
+    s += `|${' '.repeat(80)}|\n`
+    s += cow.split("\n").map(l => `|    ${l}${' '.repeat(76-l.length)}|`).join("\n")
+    s += `\n`
+    s += `|${' '.repeat(80)}|\n`
+    s += `\\${'-'.repeat(80)}/\n`
+    term.textContent = s
+
+    talkingFrame++
+    talkingFrame %= text.length
+
+    return
+
+  }
+
 
   for(let y = 0; y<height; y++){
 
@@ -75,8 +137,11 @@ const mainLoop = (timestamp) => {
 }
 
 const onKeydown = (ev) => {
-  log.textContent = `Pulsaste "${ev.key}" y nada...\nEn fin, es sólo una maqueta`
-  console.log(ev);
+  //log.textContent = `Pulsaste "${ev.key}" y nada...\nEn fin, es sólo una maqueta`
+  face = faces[Math.floor(Math.random()*faces.length)]
+  talking = true
+  talkingFrame = 0
+  console.log(face,ev)
 }
 
 document.addEventListener("DOMContentLoaded", () => {
