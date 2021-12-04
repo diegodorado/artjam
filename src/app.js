fb7221216ab57898b2e527b63f3dcb961c2f3e8f
Diego Dorado <diegodorado@gmail.com>
2021-12-04 13:09:32 -0300

    Remove cowsay dependency

--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
 import intro from "./intro.json"
-import cowsay from "cowsay-browser"
-
+import {
+  doge,
+  tiger,
+} from './avatars'
 
 const raf = window.requestAnimationFrame
 
 const height = intro[0].length
 const width = intro[0][0].length
-const board = new Array(height*width)
 
 const term = document.getElementById('term')
-const log = document.getElementById('log')
 const introMessage = "    Acaricia una tecla    "
 
 const maxFPS = 10
@@ -21,8 +21,88 @@ let frameIndex = 0
 let talking = false
 let talkingFrame = 0
 
-const faces = ['bud-frogs', 'bunny', 'cheese', 'chick', 'cower', 'daemon', 'default', 'doge', 'dragon-and-cow', 'dragon', 'elephant-in-snake', 'elephant', 'eyes', 'flaming-sheep', 'kiss', 'kitty', 'koala', 'kosh', 'luke-koala', 'mech-and-cow', 'meow', 'moofasa', 'moose', 'ren', 'rooster', 'satanic', 'sheep', 'skeleton', 'small', 'squirrel', 'stegosaurus', 'stimpy', 'supermilker', 'turkey', 'turtle', 'tux', 'vader-koala', 'vader', 'www']
-let face = faces[Math.floor(Math.random()*faces.length)]
+function format (text, wrap, delimiters) {
+	var lines = split(text, wrap);
+	var maxLength = max(lines);
+
+	var balloon;
+	if (lines.length === 1) {
+		balloon = [
+			" " + top(maxLength), 
+			delimiters.only[0] + " " + lines[0] + " " + delimiters.only[1],
+			" " + bottom(maxLength)
+		];
+	} else {
+		balloon = [" " + top(maxLength)];
+
+		for (var i = 0, len = lines.length; i < len; i += 1) {
+			var delimiter;
+
+			if (i === 0) {
+				delimiter = delimiters.first;
+			} else if (i === len - 1) {
+				delimiter = delimiters.last;
+			} else {
+				delimiter = delimiters.middle;
+			}
+
+			balloon.push(delimiter[0] + " " + pad(lines[i], maxLength) + " " + delimiter[1]);
+		}
+
+		balloon.push(" " + bottom(maxLength));
+	}
+
+	return balloon.join("\n"); //os.EOL
+}
+
+function split (text, wrap) {
+	text = text.replace(/\r\n?|[\n\u2028\u2029]/g, "\n").replace(/^\uFEFF/, '');
+
+	var lines = [];
+	if (!wrap) {
+		lines = text.split("\n");
+	} else {
+		var start = 0;
+		while (start < text.length) {
+			var nextNewLine = text.indexOf("\n", start);
+
+			var wrapAt = Math.min(start + wrap, nextNewLine === -1 ? text.length : nextNewLine);
+
+			lines.push(text.substring(start, wrapAt));
+			start = wrapAt;
+
+			// Ignore next new line
+			if (text.charAt(start) === "\n") {
+				start += 1;
+			}
+		}
+	}
+
+	return lines;
+}
+
+function max (lines) {
+	var max = 0;
+	for (var i = 0, len = lines.length; i < len; i += 1) {
+		if (lines[i].length > max) {
+			max = lines[i].length;
+		}
+	}
+
+	return max;
+}
+
+function pad (text, length) {
+	return text + (new Array(length - text.length + 1)).join(" ");
+}
+
+function top (length) {
+	return new Array(length + 3).join("_");
+}
+
+function bottom (length) {
+	return new Array(length + 3).join("-");
+}
 
 // https://patorjk.com/software/taag/#p=display&f=AMC%203%20Liv1&t=Festival%20Placer
 const title = `
@@ -37,15 +117,44 @@ const title = `
 
 `.replaceAll('#','\\').replaceAll('*','`')
 
-const text =  `buenas, como estan?
-...
-algo así me imagino el espacio.
-un anfitrión que converse
-con el visitante
-.
-les gusta mas blanco o verde
-el texto?
+const text =  `
+    ¿Qué te calienta?                   
+    ¿Qué te da placer?                
+    ¿Por qué te gusta lo que te gusta?      
+    Con respeto y con amor.             
+    Sin tabúes y sin discriminación.        
+    Afectos, amor y responsabilidad.          
+    Disfrute, goce y deseos.                
+    Un mundo paralelo en donde no nos juzguen
+    por cómo vemos y percibimos,         
+    donde estemos en igualdad de condiciones,              
+    más allá de nuestra raza, género o edad...           
+    más acá, más cerca de nuestros placeres.       
+    Lancemos un grito orgásmico sin tapujos.      
 `
+
+const say = (text, wrap) => {
+	delimiters = {
+		first : ["/", "\\"],
+		middle : ["|", "|"],
+		last : ["\\", "/"],
+		only : ["<", ">"]
+	};
+
+	return format(text, wrap, delimiters);
+}
+
+const think = (text, wrap) => {
+	delimiters = {
+		first : ["(", ")"],
+		middle : ["(", ")"],
+		last : ["(", ")"],
+		only : ["(", ")"]
+	};
+
+	return format(text, wrap, delimiters);
+}
+
 
 
 const update = (delta) => {
@@ -60,20 +169,16 @@ const draw = () => {
   if(talking) {
 
     s += title
+    s += say(text.substr(0,talkingFrame+1), 40)
+    s += `
+         \\
+          \\
+    `
+    s += tiger
 
-    const options = {
-      text: text.substr(0,talkingFrame+1),
-      f: face,
-      //f: 'satanic', // Template for a cow, get inspiration from `./cows`
-      d: true,
-      //eyes: 'oo', // Select the appearance of the cow's eyes, equivalent to cowsay -e
-      // One of 	"b", "d", "g", "p", "s", "t", "w", "y"
-    }
-
-    let cow = cowsay.say(options)
     s += `/${'-'.repeat(80)}\\\n`
     s += `|${' '.repeat(80)}|\n`
-    s += cow.split("\n").map(l => `|    ${l}${' '.repeat(76-l.length)}|`).join("\n")
+    //s += cow.split("\n").map(l => `|    ${l}${' '.repeat(76-l.length)}|`).join("\n")
     s += `\n`
     s += `|${' '.repeat(80)}|\n`
     s += `\\${'-'.repeat(80)}/\n`
@@ -137,11 +242,8 @@ const mainLoop = (timestamp) => {
 }
 
 const onKeydown = (ev) => {
-  //log.textContent = `Pulsaste "${ev.key}" y nada...\nEn fin, es sólo una maqueta`
-  face = faces[Math.floor(Math.random()*faces.length)]
   talking = true
   talkingFrame = 0
-  console.log(face,ev)
 }
 
 document.addEventListener("DOMContentLoaded", () => {
