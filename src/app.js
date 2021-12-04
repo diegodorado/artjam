00c84037003e1dc150ba16611d5c6e247dc41375
Diego Dorado <diegodorado@gmail.com>
2021-12-04 15:32:39 -0300

    Added animated manifest

--- src/app.js
+++ src/app.js
@@ -18,70 +18,32 @@ const maxFPS = 10
 let lastFrameTimeMs = 0
 let frameIndex = 0
 
-let talking = false
-let talkingFrame = 0
+let step = 0
+let stepFrameAt = 0
 
-function format (text, wrap, delimiters) {
-	var lines = split(text, wrap);
-	var maxLength = max(lines);
-
-	var balloon;
-	if (lines.length === 1) {
-		balloon = [
-			" " + top(maxLength), 
-			delimiters.only[0] + " " + lines[0] + " " + delimiters.only[1],
-			" " + bottom(maxLength)
-		];
-	} else {
-		balloon = [" " + top(maxLength)];
-
-		for (var i = 0, len = lines.length; i < len; i += 1) {
-			var delimiter;
-
-			if (i === 0) {
-				delimiter = delimiters.first;
-			} else if (i === len - 1) {
-				delimiter = delimiters.last;
-			} else {
-				delimiter = delimiters.middle;
-			}
-
-			balloon.push(delimiter[0] + " " + pad(lines[i], maxLength) + " " + delimiter[1]);
-		}
-
-		balloon.push(" " + bottom(maxLength));
-	}
-
-	return balloon.join("\n"); //os.EOL
-}
-
-function split (text, wrap) {
-	text = text.replace(/\r\n?|[\n\u2028\u2029]/g, "\n").replace(/^\uFEFF/, '');
+const split = (text, wrap) => {
+	//text = text.replace(/\r\n?|[\n\u2028\u2029]/g, "\n").replace(/^\uFEFF/, '');
 
 	var lines = [];
-	if (!wrap) {
-		lines = text.split("\n");
-	} else {
-		var start = 0;
-		while (start < text.length) {
-			var nextNewLine = text.indexOf("\n", start);
+  var start = 0;
+  while (start < text.length) {
+    var nextNewLine = text.indexOf("\n", start);
 
-			var wrapAt = Math.min(start + wrap, nextNewLine === -1 ? text.length : nextNewLine);
+    var wrapAt = Math.min(start + wrap, nextNewLine === -1 ? text.length : nextNewLine);
 
-			lines.push(text.substring(start, wrapAt));
-			start = wrapAt;
+    lines.push(text.substring(start, wrapAt));
+    start = wrapAt;
 
-			// Ignore next new line
-			if (text.charAt(start) === "\n") {
-				start += 1;
-			}
-		}
-	}
+    // Ignore next new line
+    if (text.charAt(start) === "\n") {
+      start += 1;
+    }
+  }
 
 	return lines;
 }
 
-function max (lines) {
+const maxLine =  (lines) => {
 	var max = 0;
 	for (var i = 0, len = lines.length; i < len; i += 1) {
 		if (lines[i].length > max) {
@@ -96,128 +58,172 @@ function pad (text, length) {
 	return text + (new Array(length - text.length + 1)).join(" ");
 }
 
-function top (length) {
-	return new Array(length + 3).join("_");
-}
-
-function bottom (length) {
-	return new Array(length + 3).join("-");
-}
-
 // https://patorjk.com/software/taag/#p=display&f=AMC%203%20Liv1&t=Festival%20Placer
 const title = `
-             (                                (
-             )# )          )             (    )# ) (
-            (()/(  (    ( /((   )      ) )#  (()/( )#   )       (  (
-             /(_))))#(  )#())# /((  ( /(((_)  /(_)|(_| /(  (   ))# )(
-            (_))_/((_)#(_))((_|_))# )(_))_   (_))  _ )(_)) )# /((_|()#
-            | |_(_))((_) |_ (_))((_|(_)_| |  | _ #| ((_)_ ((_|_))  ((_)
-            | __/ -_|_-<  _|| # V // _* | |  |  _/| / _* / _|/ -_)| '_|
-            |_| #___/__/#__||_|#_/ #__,_|_|  |_|  |_#__,_#__|#___||_|
+  (                                (                       
+ )# )          )             (    )# ) (                   
+(()/(  (    ( /((   )      ) )#  (()/( )#   )       (  (   
+ /(_))))#(  )#())# /((  ( /(((_)  /(_)|(_| /(  (   ))# )(  
+(_))_/((_)#(_))((_|_))# )(_))_   (_))  _ )(_)) )# /((_|()# 
+| |_(_))((_) |_ (_))((_|(_)_| |  | _ #| ((_)_ ((_|_))  ((_)
+| __/ -_|_-<  _|| # V // _* | |  |  _/| / _* / _|/ -_)| '_|
+|_| #___/__/#__||_|#_/ #__,_|_|  |_|  |_#__,_#__|#___||_|  
 
 `.replaceAll('#','\\').replaceAll('*','`')
 
-const text =  `
-    ¿Qué te calienta?                   
-    ¿Qué te da placer?                
-    ¿Por qué te gusta lo que te gusta?      
-    Con respeto y con amor.             
-    Sin tabúes y sin discriminación.        
-    Afectos, amor y responsabilidad.          
-    Disfrute, goce y deseos.                
-    Un mundo paralelo en donde no nos juzguen
-    por cómo vemos y percibimos,         
-    donde estemos en igualdad de condiciones,              
-    más allá de nuestra raza, género o edad...           
-    más acá, más cerca de nuestros placeres.       
-    Lancemos un grito orgásmico sin tapujos.      
+const manifest = [
 `
+¿Qué te calienta?                   
+¿Qué te da placer?                
+¿Por qué te gusta lo que te gusta?      
+`,
+`
+Con respeto y con amor.             
+Sin tabúes y sin discriminación.        
+`,
+`
+Afectos, amor y responsabilidad.          
+Disfrute, goce y deseos.                
+`,
+`
+Un mundo paralelo en donde no nos juzguen
+por cómo vemos y percibimos,         
+donde estemos en igualdad de condiciones,              
+`,
+`
+más allá de nuestra raza, género o edad...           
+más acá, más cerca de nuestros placeres.       
+`,
+`
+Lancemos un grito orgásmico liberador.      
+`
+]
 
-const say = (text, wrap) => {
-	delimiters = {
-		first : ["/", "\\"],
-		middle : ["|", "|"],
-		last : ["\\", "/"],
-		only : ["<", ">"]
-	};
-
-	return format(text, wrap, delimiters);
+const delimiters = {
+  first : ["/", "\\"],
+  middle : ["|", "|"],
+  last : ["\\", "/"],
 }
 
-const think = (text, wrap) => {
-	delimiters = {
-		first : ["(", ")"],
-		middle : ["(", ")"],
-		last : ["(", ")"],
-		only : ["(", ")"]
-	};
+const say = (text, wrap, maxchars) => {
 
-	return format(text, wrap, delimiters);
+	const lines = split(text, wrap);
+	const maxLength = maxLine(lines);
+
+	const top  = new Array(maxLength + 3).join("_")
+	const balloon = [top];
+
+  let charsLeft = maxchars
+  for (var i = 0, len = lines.length; i < len; i += 1) {
+    var delimiter;
+
+    if (i === 0) {
+      delimiter = delimiters.first
+    } else if (i === len - 1) {
+      delimiter = delimiters.last
+    } else {
+      delimiter = delimiters.middle
+    }
+
+    balloon.push(delimiter[0] + " " + pad(lines[i].substr(0,charsLeft), maxLength) + " " + delimiter[1])
+    charsLeft -= lines[i].length
+  }
+
+	const bottom = new Array(maxLength + 3).join("-")
+  balloon.push(bottom)
+
+	return balloon.join("\n")
 }
-
-
 
 const update = (delta) => {
 }
 
+const moveRight = (text, count)  => {
+	const margin  = new Array(count).join(" ")
+  return text.split("\n").map(l => margin + l).join("\n")
+}
+
+const sayManifest = (index, animated) => {
+  let s = ""
+  s += title
+  let l = manifest[index].length
+  const stepFrame = frameIndex-stepFrameAt
+  if(animated){
+    // autoadvance step if reached the animation end
+    if ( stepFrame > l){
+      console.log("advance")
+      step++
+      stepFrameAt = frameIndex
+    }
+
+    l = Math.min(l,stepFrame)
+  }
+  s += say(manifest[index], 50, l+1)
+  s += `
+   \\
+    \\
+     \\
+  `
+  s += tiger
+  s += `\n\n\n    ${(step%2 === 0) ? '( tocame una tecla continuar )' : ''}    \n`
+  return s
+}
+
 const draw = () => {
-  const index = frameIndex % intro.length
-  const frame = intro[index]
 
   let s = ""
 
-  if(talking) {
+  switch(step){
 
-    s += title
-    s += say(text.substr(0,talkingFrame+1), 40)
-    s += `
-         \\
-          \\
-    `
-    s += tiger
+    // intro
+    case 0:
 
-    s += `/${'-'.repeat(80)}\\\n`
-    s += `|${' '.repeat(80)}|\n`
-    //s += cow.split("\n").map(l => `|    ${l}${' '.repeat(76-l.length)}|`).join("\n")
-    s += `\n`
-    s += `|${' '.repeat(80)}|\n`
-    s += `\\${'-'.repeat(80)}/\n`
-    term.textContent = s
+      const index = frameIndex % intro.length
+      const frame = intro[index]
 
-    talkingFrame++
-    talkingFrame %= text.length
+      for(let y = 0; y<height; y++){
 
-    return
+        let row = frame[y]
 
-  }
-
-
-  for(let y = 0; y<height; y++){
-
-    let row = frame[y]
-
-    // add message if intro was played once
-    if(frameIndex > intro.length && Math.abs(Math.floor(height/2)-y) <= 2){
-      row = ""
-      let i = 0
-      for(let x = 0; x< width; x++){
-        if(Math.floor(frameIndex/16) % 2 && Math.abs(Math.floor(width/2)-x) <= introMessage.length/2){
-          if(Math.floor(height/2) === y && i < introMessage.length){
-            row += introMessage[i++]
-          }
-          else{
-            row += " "
+        // add message if intro was played once
+        if(frameIndex > intro.length && Math.abs(Math.floor(height/2)-y) <= 2){
+          row = ""
+          let i = 0
+          for(let x = 0; x< width; x++){
+            if(Math.floor(frameIndex/16) % 2 && Math.abs(Math.floor(width/2)-x) <= introMessage.length/2){
+              if(Math.floor(height/2) === y && i < introMessage.length){
+                row += introMessage[i++]
+              }
+              else{
+                row += " "
+              }
+            }
+            else{
+              row += frame[y][x]
+            }
           }
         }
-        else{
-          row += frame[y][x]
-        }
+        s += row
+        s += "\n"
       }
-    }
+      break
 
-    s += row
-    s += "\n"
-
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+    case 7:
+    case 8:
+    case 9:
+    case 10:
+    case 11:
+    case 12:
+      s += sayManifest(Math.round(step/2)-1, step%2)
+      break
+    default:
+      s += "... fin de la maqueta  =( ..."
   }
 
   term.textContent = s
@@ -242,8 +248,8 @@ const mainLoop = (timestamp) => {
 }
 
 const onKeydown = (ev) => {
-  talking = true
-  talkingFrame = 0
+  step++
+  stepFrameAt = frameIndex
 }
 
 document.addEventListener("DOMContentLoaded", () => {
